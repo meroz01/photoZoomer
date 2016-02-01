248942fa7c0f67dec1e9b44a9acc5a430444ae7f
Discordant <niezgodny@hotmail.com>
2016-02-01 21:42:46 +0100

    positions

--- app.js
+++ app.js
@@ -11,20 +11,41 @@ zoomer.elements = function() {
         d.x2 = el.offsetLeft + el.offsetWidth;
         d.y2 = el.offsetTop + el.offsetHeight;
         
-        console.log(d)
-        
         return d;
     }
     
     return function() {
-        var elements = document.getElementsByClassName(zoomer.name);
+        var elements = document.getElementsByClassName(zoomer.name),
+            dim,
+            mouseX,
+            mouseY,
+            heightPercent,
+            widthPercent;
+            
         for(var i in elements) {
             if(typeof elements[i] === 'object') {
                 (function(el){
-                    el.addEventListener('mouseover', function() {
-                        console.log('in');
-                        //console.log(getDimentions(elements[i]));
-                        getDimentions(el);
+                    el.addEventListener('mousemove', function() {
+                        if (dim !== getDimentions(el)) {
+                            dim = getDimentions(el);
+                            
+                            mouseX = event.clientX;
+                            mouseY = event.clientY;
+                            
+                            // console.log('x: ' + mouseX + ' x1: ' + dim.x1 + ' x2: ' + dim.x2);
+                            // console.log('y ' + mouseY + ' y1: ' + dim.y1 + ' y2: ' + dim.y2);
+                            
+                            if(mouseX > dim.x1 && mouseX < dim.x2) {
+                                widthPercent = mouseX/(dim.x2 - dim.x1);
+                                console.log('x: ' + widthPercent);
+                            }
+                            
+                            if(mouseY > dim.y1 && mouseY < dim.y2) {
+                                heightPercent = mouseY/(dim.y2 - dim.y1);
+                                console.log('y: ' + heightPercent);
+                            }
+                        }
+                        
                     })
                 })(elements[i]);
                 
