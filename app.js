c554523d0cff2a8cdc909d02ae5f8d7c38ed9080
Daniel Majewski <dm470876@wmservice.corpnet1.com>
2016-02-02 12:17:58 +0100

    fixed window scroll issue

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ zoomer.elements = function() {
     function getDimentions(el) {
         var d = {};
         
-        d.x1 = el.offsetLeft;
-        d.y1 = el.offsetTop;
-        d.x2 = el.offsetLeft + el.offsetWidth;
-        d.y2 = el.offsetTop + el.offsetHeight;
+        d.x1 = el.offsetLeft - window.pageXOffset;
+        d.y1 = el.offsetTop - window.pageYOffset;
+        d.x2 = el.offsetLeft + el.offsetWidth - window.pageXOffset;
+        d.y2 = el.offsetTop + el.offsetHeight - window.pageYOffset;
+        
+        // console.log(+ window.pageXOffset);
+        // console.log(+ window.pageYOffset);
         
         return d;
     }
@@ -19,8 +22,8 @@ zoomer.elements = function() {
             dim,
             mouseX,
             mouseY,
-            heightPercent,
-            widthPercent;
+            verticalPercent,
+            horizontalPercent;
             
         for(var i in elements) {
             if(typeof elements[i] === 'object') {
@@ -32,20 +35,27 @@ zoomer.elements = function() {
                             mouseX = event.clientX;
                             mouseY = event.clientY;
                             
+                            
                             // console.log('x: ' + mouseX + ' x1: ' + dim.x1 + ' x2: ' + dim.x2);
                             // console.log('y ' + mouseY + ' y1: ' + dim.y1 + ' y2: ' + dim.y2);
                             
-                            if(mouseX > dim.x1 && mouseX < dim.x2) {
-                                widthPercent = mouseX/(dim.x2 - dim.x1);
-                                console.log('x: ' + widthPercent);
-                            }
-                            
-                            if(mouseY > dim.y1 && mouseY < dim.y2) {
-                                heightPercent = mouseY/(dim.y2 - dim.y1);
-                                console.log('y: ' + heightPercent);
+                            if(mouseX > dim.x1 && mouseX < dim.x2 && mouseY > dim.y1 && mouseY < dim.y2) {
+                                horizontalPercent = (mouseX-dim.x1)/(dim.x2 - dim.x1);
+                                verticalPercent = (mouseY-dim.y1)/(dim.y2 - dim.y1);
+                                //  console.log('x: ' + horizontalPercent);
+                                //  console.log('y: ' + verticalPercent);
+                                if(el.style.backgroundSize !== 'initial') {
+                                    el.style.backgroundSize = 'initial';
+                                }
+                                el.style.backgroundPositionX = (horizontalPercent * 100) + '%';
+                                el.style.backgroundPositionY = (verticalPercent * 100) + '%';
                             }
                         }
                         
+                    })
+                    
+                    el.addEventListener('mouseout', function() {
+                        el.style.backgroundSize = 'cover';
                     })
                 })(elements[i]);
                 
